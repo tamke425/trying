53a536fff6f4e26c857e7992c03c1e73d4d5c1f7
tamke425 <tamke425@gmail.com>
2022-09-25 16:36:20 +0200

    tame has commited for the first time

---
+++ print.py
@@ -0,0 +1,11 @@
+#def take_numbers(num1,num2):
+   # if num1 >= num2:
+    #    return num1
+    #else:
+     #   return num2
+
+#def add_numbers(num3):
+    #x=num3 + take_numbers(num1,num2)
+    #return x
+print("hi tame")
+
